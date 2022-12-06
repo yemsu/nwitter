ad1b47732b15c5eff5b9d09344ff16de37e379b8
정수진 <=>
2022-12-06 17:53:31 +0900

    feat: 네비게이션

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,18 +1,22 @@
 import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import Auth from 'routes/Auth'
 import Home from 'routes/Home'
+import Profile from 'routes/Profile'
+import Navigation from './Navigation'
 
 const AppRouter = ({ isLoggedIn }) => {
 
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
           // exact path="/" 프롭스를 전달하면 누이터에 처음 접속했을 때 Home or Auth 컴포넌트를 보여줄 수 있다.
-          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/" element={<Home />} />
         ) : (
-          <Route exact path="/" element={<Auth />}></Route>
+          <Route exact path="/" element={<Auth />} />
         )}
+        <Route exact path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   )
