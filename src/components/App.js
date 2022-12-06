b9abd9458fdef6fdb9eb55391d2881d2cdbc2a53
정수진 <=>
2022-12-06 15:03:01 +0900

    refactor: 라우터와 관련되지 않은 로그인 state는 App.js로 분리

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import AppRouter from "./Router";
+import { useState } from 'react'
 
 function App() {
-  return <AppRouter />;
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  return <AppRouter isLoggedIn={isLoggedIn} />;
 }
 
 export default App;
