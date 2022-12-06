ee9d44e8b826acde59ca4689a979180346daafa0
정수진 <=>
2022-12-06 14:53:22 +0900

    3장 라우터 적용

---
+++ src/components/Router.js
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+import Auth from '../routes/Auth'
+import Home from '../routes/Home'
+
+const AppRouter = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  return (
+    <Router>
+      <Routes>
+        {isLoggedIn ? (
+          // exact path="/" 프롭스를 전달하면 누이터에 처음 접속했을 때 Home or Auth 컴포넌트를 보여줄 수 있다.
+          <Route exact path="/" element={<Home />}></Route>
+        ) : (
+          <Route exact path="/" element={<Auth />}></Route>
+        )}
+      </Routes>
+    </Router>
+  )
+}
+
+export default AppRouter
