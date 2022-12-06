45462fb678309d8bf34759f8a43e2193d301571c
정수진 <=>
2022-12-06 18:02:56 +0900

    feat: 프로필에 로그아웃 버튼 삽입 및 라우터 리다이렉트 설정

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Navigate, Route, Routes } from 'react-router-dom'
 import Auth from 'routes/Auth'
 import Home from 'routes/Home'
 import Profile from 'routes/Profile'
@@ -8,15 +8,18 @@ const AppRouter = ({ isLoggedIn }) => {
 
   return (
     <Router>
-      {isLoggedIn && <Navigation />}
+      {!isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
           // exact path="/" 프롭스를 전달하면 누이터에 처음 접속했을 때 Home or Auth 컴포넌트를 보여줄 수 있다.
-          <Route exact path="/" element={<Home />} />
+          <>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/profile" element={<Profile />} />
+          </>
         ) : (
           <Route exact path="/" element={<Auth />} />
         )}
-        <Route exact path="/profile" element={<Profile />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   )
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,11 @@
-const Profile = () => <span>Profile</span>
+import { auth } from 'fBase'
+
+const Profile = () => {
+  const onLogOutClick = () => auth.signOut()
+
+  return (
+    <button onClick={onLogOutClick}>Log Out</button>
+  )
+}
 
 export default Profile
